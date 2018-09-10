b85c086927c871cc5694cd52835c3080305b2eed
mamedov94 <mamedov094@bk.ru>
2018-09-10 21:20:44 +0300

    sec

--- Spirit/js/main.js
+++
@@ -1,9 +0,0 @@
-/* Button down */
-
-$(document).ready(function() {
-	$('.scrl-down').click(function() {
-		$('html,body').animate({scrollTop: $('header').height()+ 85}, 600);
-
-		return false
-	});
-});
